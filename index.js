e3037a0a6f6844cddaa0f3e80dcc99bceeaf2471
Sherwin Abraham <sherwinabraham@gmail.com>
2023-07-26 12:05:10 -0500

    Fixed text color and added end message.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const { Circle, Triangle, Square } = require('./lib/shapes');
 const Svg = require('./lib/svg');
 const inquirer = require("inquirer");
-const fs=require("fs")
+const fs = require("fs")
 function startMenu() {
     inquirer.prompt([
         {
             type: "input",
-            message: "enter text",
+            message: "Enter your text, please.",
             name: "logo_text"
         },
         {
             type: "list",
             choices: ["red", "yellow", "blue", "green", "purple"],
-            message: "enter text color",
+            message: "Please enter the text color.",
             name: "logo_text_color"
         },
         {
             type: "list",
             choices: ["Triangle", "Square", "Circle"],
-            message: "enter shape",
+            message: "Enter your shape.",
             name: "logo_shape"
         },
         {
             type: "list",
             choices: ["Red", "Yellow", "Blue", "Green", "Purple"],
-            message: "enter shape color",
+            message: "Enter your shape color.",
             name: "logo_shape_color"
         },
 
@@ -44,12 +44,13 @@ function startMenu() {
             }
             myShape.setColor(response.logo_shape_color)
             var myLogo = new Svg()
-            myLogo.setText(response.logo_text, response.logo_color)
+            myLogo.setText(response.logo_text, response.logo_text_color)
             myLogo.setShape(myShape)
             fs.writeFileSync("./logo.svg",myLogo.render(),function(error){
                 if(error)throw error
             })
+            console.log("Generated logo.svg")
         })
-
+        
 }
 startMenu()
